72040508d13579298d2559deee8d2881550c82f6
2013-11390 <jeonghwan1031@mail.snu.ac.kr>
2020-02-14 05:04:17 +0900

    Fix: eslintrc and packages for linting correctly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended",
-        
-        "airbnb-base"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: [
+    'node',
+    'jest',
+  ],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
 };
